4f5956eb3983f32d0f98cc2e93b0599b7ace4973
taylorvercillo <taylornvcasey@gmail.com>
2022-06-16 22:12:38 -0600

    working on form inputs

--- src/components/NewMemory.js
+++ src/components/NewMemory.js
@@ -1,12 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 
 function NewMemory(){
 
+    const [name, setName] = useState("")
+    const [body, setBody] = useState("")
+    const [date, setDate] = useState("")
+
+
+
     return (
         <form>
-            <input type="text" name="name" placeholder="Kid's Name" />
-            <input type="text" name="body" placeholder="Memory Description" />
-            <input type="text" name="date" placeholder="Date" />
+            <input type="text" name="name" placeholder="Kid's Name" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" name="body" placeholder="Memory Description" value={body} onChange={(e) => setBody(e.target.value)} />
+            <input type="text" name="date" placeholder="Date" value={date} onChange={(e) => setDate(e.target.value)} />
             <button>Add New Memory</button>
         </form>
     )
